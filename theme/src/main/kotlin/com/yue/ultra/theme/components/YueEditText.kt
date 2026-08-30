dd86ed492bb841289af61b58275365ff1bfeab46
YueUltra <yueultra@theme.com>
2026-08-30 09:40:01 +0800

    修复：PasswordVisualTransformation导入

--- theme/src/main/kotlin/com/yue/ultra/theme/components/YueEditText.kt
+++ theme/src/main/kotlin/com/yue/ultra/theme/components/YueEditText.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import PasswordVisualTransformation
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
